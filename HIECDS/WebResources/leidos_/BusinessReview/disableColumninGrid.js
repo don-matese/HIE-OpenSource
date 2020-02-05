58d55d265d55c00871064ea79cc51188eb229b76
Automatic Build <mauro.masucci@leidodscloud.onmicrosoft.com>
2020-02-05 19:32:47 +0000

    solution init

---
+++ HIECDS/WebResources/leidos_/BusinessReview/disableColumninGrid.js
@@ -0,0 +1,21 @@
+var Leidos = window.Leidos || {};
+
+Leidos.disableFieldEdit = function(context, fieldName)
+{
+    context.getFormContext().getData().getEntity().attributes.forEach(function (attr) {
+        if (attr.getName() === fieldName) {
+            attr.controls.forEach(function (c) {
+                c.setDisabled(true);
+            })
+        }
+    });
+}
+
+
+Leidos.gridRowSelected =function (context) {   
+    Leidos.disableFieldEdit(context, "leidos_missingscores");
+}
+
+Leidos.gridRowSelectedAnswer =function(context) {      
+    Leidos.disableFieldEdit(context, "leidos_previousratingvalue");
+}
